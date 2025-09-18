418cdd7b70cadbf0276c4b2c790052cf13946266
shaquille neil <shaquille-36@hotmail.com>
2025-09-18 12:26:57 -0400

    Initial Commit

---
+++ Calculator/CalculatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  CalculatorApp.swift
+//  Calculator
+//
+//  Created by Shaquille O Neil on 2025-09-18.
+//
+
+import SwiftUI
+
+@main
+struct CalculatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
